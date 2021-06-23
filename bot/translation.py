f2a5151c73ebfcb68c7622867e5445efc757352d
refreshprabha <85902893+refreshprabha@users.noreply.github.com>
2021-06-24 01:17:04 +0530

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>Contact @TamilRasanai
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>How to Get me working?</u></i></b>
+Coding
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> CineCube Bot </code>
+
+"""
